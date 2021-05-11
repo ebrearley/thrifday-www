80c140ff9a0ecbca3bda61711abb913b39114eb2
Eric Brearley <eric@ericbrearley.com>
2021-05-12 03:54:39 +1000

    Change logout link to a button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Button, Link as ChakraLink } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
 import { TextLink } from './TextLink';
@@ -8,7 +8,11 @@ export const Sidebar = () => (
   <Box borderRightWidth="1px" flexShrink={1} width="15rem">
     <Box position="fixed" padding="1rem" width="15rem" display="flex" flexDirection="column" justifyContent="space-between" height="calc(100% - 4rem)">
       <Box as="nav">Sidebar nav items</Box>
-      <TextLink href="/logout">Log out</TextLink>
+      <Link href="/logout">
+        <Button colorScheme="teal" variant="ghost">
+          Log out
+        </Button>
+      </Link>
     </Box>
   </Box>
 );
